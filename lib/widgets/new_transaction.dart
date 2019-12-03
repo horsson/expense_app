0900eb40a1fb16d057933fb1c421a576e61adec5
horsson <horsson@gmail.com>
2019-12-03 23:49:03 +0100

    add bar button and floating button.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,15 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void _submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -23,14 +32,12 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
-              onPressed: () {
-                print(titleController.text);
-                addTx(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: _submitData,
               textColor: Colors.purple,
             ),
           ],
